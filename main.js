ff228eb93d9364543c2d3682ef746d92d854a10f
Luay Alali <luayalali@gmail.com>
2020-09-17 18:36:44 -0700

    displayModal function added. And associated data inserted into modalHTML

--- main.js
+++ main.js
@@ -39,3 +39,24 @@ function displayEmployees(eData){
     });
     gridContainer.innerHTML = employeeHTML;
 }
+
+function displayModal(index) {
+    let { name, dob, phone, email, location: {city, street, state, postcode }, picture } = employees[index];
+    let date = new Date(dob.date);
+
+    const modalHTML = `
+    <img src="${picture.large}" alt="" class="avatar">
+    <div class="text-container">
+        <h2 class="name">${name.first} ${name.last}</h2>
+        <p class="email">${email}</p>
+        <p class="address">${city}</p>
+        <hr />
+        <p>${phone}</p>
+        <p class="address">${address}</p>
+        <p>Birthday: ${date.getMonth()}/${date.getDate}/${date.getFullYear()}</p>
+    </div>
+    `;
+
+    overlay.classList.remove('hidden');
+    modalContainer.innerHTML = modalHTML;
+}
